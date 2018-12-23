ead1dcaf7f4b1b7c7c4562f590ffa81adca867d7
John Barrett <johnjbarrett22@gmail.com>
2018-12-22 16:47:37 -0800

    Minor tweaks

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -23,15 +23,15 @@ export class AppComponent {
   }
 
   join(){
-    this._chatService.joinRoom({user:this.user, room:this.room});
+    this._chatService.joinRoom({user: this.user, room: this.room});
   }
   
   leave(){
-    this._chatService.leaveRoom({user:this.user, room:this.room});
+    this._chatService.leaveRoom({user: this.user, room: this.room});
   }
 
   sendMessage(){
-    this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
+    this._chatService.sendMessage({user: this.user, room: this.room, message: this.messageText});
     this.messageText = "";
   }
 
