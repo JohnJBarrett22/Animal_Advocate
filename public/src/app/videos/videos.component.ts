94d9e8f819d53c7c51fec0eb42316ee13426f829
John Barrett <johnjbarrett22@gmail.com>
2019-02-17 00:49:29 -0800

    Fixed SCSS issue that was causing problems with my styling

--- public/src/app/videos/videos.component.ts
+++ public/src/app/videos/videos.component.ts
@@ -10,30 +10,30 @@ export class VideosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener("DOMContentLoaded", function() {
-      let div, n, v = document.getElementsByClassName("youtube-player");
-      for (n = 0; n < v.length; n++) {
-        div = document.createElement("div");
-        div.setAttribute("data-id", v[n].id);
-        div.setAttribute("class", "middleDiv");
-        div.innerHTML = loadThumbnail(v[n].id);
-        div.onclick = loadIframe;
-        v[n].appendChild(div);
-      }
-    });
+    // document.addEventListener("DOMContentLoaded", function() {
+    //   let div, n, v = document.getElementsByClassName("youtube-player");
+    //   for (n = 0; n < v.length; n++) {
+    //     div = document.createElement("div");
+    //     div.setAttribute("data-id", v[n].id);
+    //     div.setAttribute("class", "middleDiv");
+    //     div.innerHTML = loadThumbnail(v[n].id);
+    //     div.onclick = loadIframe;
+    //     v[n].appendChild(div);
+    //   }
+    // });
 
-    function loadThumbnail(id) {
-      let thumb = '<img class="thumbNail" src="https://i.ytimg.com/vi/ID/hqdefault.jpg">', play = '<div class="play"></div>';
-      return thumb.replace("ID", id) + play;
-    }
+    // function loadThumbnail(id) {
+    //   let thumb = '<img class="thumbNail" src="https://i.ytimg.com/vi/ID/hqdefault.jpg">', play = '<div class="play"></div>';
+    //   return thumb.replace("ID", id) + play;
+    // }
 
-    function loadIframe() {
-      let iframe = document.createElement("iframe");
-      let embed = "https://www.youtube.com/embed/ID?autoplay=1";
-      iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-      iframe.setAttribute("frameborder", "0");
-      iframe.setAttribute("allowfullscreen", "1");
-      this.parentNode.replaceChild(iframe, this);
-    }
+    // function loadIframe() {
+    //   let iframe = document.createElement("iframe");
+    //   let embed = "https://www.youtube.com/embed/ID?autoplay=1";
+    //   iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+    //   iframe.setAttribute("frameborder", "0");
+    //   iframe.setAttribute("allowfullscreen", "1");
+    //   this.parentNode.replaceChild(iframe, this);
+    // }
   }
 }
