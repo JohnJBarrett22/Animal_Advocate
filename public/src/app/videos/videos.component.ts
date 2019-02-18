e092f2fd59b8cf308f6454ccc770d79675b37d0f
John Barrett <johnjbarrett22@gmail.com>
2019-02-17 18:49:43 -0800

    Hit a wall with video component, as modals aren't working as intended. Added addtional functionality and have several pieces commented out as I work through the issues.

--- public/src/app/videos/videos.component.ts
+++ public/src/app/videos/videos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-videos',
@@ -6,34 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./videos.component.css']
 })
 export class VideosComponent implements OnInit {
+  url: any;
+  videoUrl: any;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
+
+  @ViewChild('content') public contentModal;
+  public name: string;
 
   ngOnInit() {
-    // document.addEventListener("DOMContentLoaded", function() {
-    //   let div, n, v = document.getElementsByClassName("youtube-player");
-    //   for (n = 0; n < v.length; n++) {
-    //     div = document.createElement("div");
-    //     div.setAttribute("data-id", v[n].id);
-    //     div.setAttribute("class", "middleDiv");
-    //     div.innerHTML = loadThumbnail(v[n].id);
-    //     div.onclick = loadIframe;
-    //     v[n].appendChild(div);
-    //   }
-    // });
+  }
 
-    // function loadThumbnail(id) {
-    //   let thumb = '<img class="thumbNail" src="https://i.ytimg.com/vi/ID/hqdefault.jpg">', play = '<div class="play"></div>';
-    //   return thumb.replace("ID", id) + play;
-    // }
-
-    // function loadIframe() {
-    //   let iframe = document.createElement("iframe");
-    //   let embed = "https://www.youtube.com/embed/ID?autoplay=1";
-    //   iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-    //   iframe.setAttribute("frameborder", "0");
-    //   iframe.setAttribute("allowfullscreen", "1");
-    //   this.parentNode.replaceChild(iframe, this);
-    // }
+  show(id:string){
+      this.name = id;
+      this.url = 'https://www.youtube.com/embed/' + this.name;
+      console.log(this.url);
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+      console.log(this.videoUrl)
+      this.contentModal.show();
   }
 }
